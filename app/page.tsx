3b9fe4264fd64d0d4d04ac32e17bc3c8a0e7013f
wassii-khan <150877571+wassii-khan-git@users.noreply.github.com>
2025-05-30 19:59:40 +0500

    refactor: update layout and home page to improve structure and content display

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
-import Navbar from "./(components)/navbar";
-import Form from "./home/form";
+import NoteCard from "@/components/ui/notes/note-card";
+
 // Home Page
-export default function Home() {
+export default async function Home() {
   return (
-    <>
-      {/* Navbar */}
-      <Navbar />
-      <div className="container mx-auto p-4  mt-24">
-        {/* Tables */}
-        <Form />
-      </div>
-    </>
+    <div className="container mx-auto p-4 mt-10">
+      {/* add note */}
+      <NoteCard />
+    </div>
   );
 }
