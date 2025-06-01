32e32a861d328c7e16a3860681a4f75a3ec11114
wassii-khan <wassiikhan933@gmail.com>
2025-06-01 16:38:19 +0500

    feat: remove Note validation schema and related function

--- lib/actions/notes.actions.ts
+++
@@ -1,9 +0,0 @@
-// form data validation
-import { z } from "zod";
-
-export const Note = z.object({
-  title: z.string(),
-  note: z.string(),
-});
-
-export const AddNote = async (formdata: FormData) => {};
