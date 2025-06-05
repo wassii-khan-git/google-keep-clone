2f58c6a96181aaf9f17e6d1eecb7b90465fbd0c9
wassii-khan <150877571+wassii-khan-git@users.noreply.github.com>
2025-06-05 15:34:06 +0500

    feat: update the web layout

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -1,6 +1,5 @@
 import "@/app/globals.css";
 import { Poppins } from "next/font/google";
-import { ToastContainer } from "react-toastify";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -10,22 +9,7 @@ const poppins = Poppins({
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="eng">
-      <body className={poppins.className}>
-        {children}
-        {/* toast container */}
-        <ToastContainer
-          position="top-right"
-          autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="colored"
-        />
-      </body>
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }
