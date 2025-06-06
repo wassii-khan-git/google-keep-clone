89cd2a89fc062150af7b64eb0829a709027ebb6a
wassii-khan <wassiikhan933@gmail.com>
2025-06-06 12:32:38 +0500

    Refactor code structure for improved readability and maintainability

--- components/signup-form.tsx
+++ components/signup-form.tsx
@@ -50,13 +50,19 @@ export function SignupForm({
   // store user
   const SignUp = async (values: SignupProps) => {
     try {
-      const response = await fetch(`http://localhost:3000/api/auth/signup`, {
-        method: "POST",
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify(values),
-      });
+      if (process.env.NEXT_PUBLIC_API_URL === undefined) {
+        throw new Error("API URL is not defined");
+      }
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/signup`,
+        {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(values),
+        }
+      );
 
       const data = await response.json();
       console.log(data);
--- middleware.ts
+++ middleware.ts
@@ -2,21 +2,16 @@ import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(req: NextRequest) {
-  // GET THE token
-  const token = await getToken({
-    req,
-    secret: process.env.AUTH_SECRET,
-  });
-
-  // extract the pathname
+  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
   const { pathname } = req.nextUrl;
 
-  // if there is no token and access the dashboard redirect to the login or /
+  console.log("Token:", token);
+  console.log("Pathname:", pathname);
+
   if (pathname.startsWith("/dashboard") && !token) {
     return NextResponse.redirect(new URL("/", req.url));
   }
 
-  // if there is token
   if ((pathname === "/" || pathname === "/signup") && token) {
     return NextResponse.redirect(new URL("/dashboard", req.url));
   }
