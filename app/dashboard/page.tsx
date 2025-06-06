e2b383cea68e10183734a6f54eb14bfb3dc3a50f
wassii-khan <wassiikhan933@gmail.com>
2025-06-06 23:27:37 +0500

    fix: change Home component to async for improved data handling

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,7 @@
-"use client";
 import Note from "@/components/ui/notes/note";
 
 // Home Page
-export default function Home() {
+export default async function Home() {
   return (
     <div className="container mx-auto p-4 mt-10">
       {/* add note */}
