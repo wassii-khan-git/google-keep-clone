5651a838f5cc9fb75329275c65bc31852a862082
wassii-khan <wassiikhan933@gmail.com>
2025-06-06 23:27:46 +0500

    fix: convert Page component to async and handle session authentication with redirect

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -1,6 +1,12 @@
 import { LoginForm } from "@/components/login-form";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+  if (session) {
+    redirect("/dashboard");
+  }
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
