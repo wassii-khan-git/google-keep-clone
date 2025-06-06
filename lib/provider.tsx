13208973c7227a8fbe8d35f894ea1413f988211c
wassii-khan <wassiikhan933@gmail.com>
2025-06-06 17:18:11 +0500

    refactor: remove unused React import for cleaner code

--- lib/provider.tsx
+++ lib/provider.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-import React from "react";
 
+// Auth Provider
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   return <SessionProvider>{children}</SessionProvider>;
 };
