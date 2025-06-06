077100c081d20b0993bd7720235e80a05b395f55
wassii-khan <wassiikhan933@gmail.com>
2025-06-06 11:54:45 +0500

    feat: add Toaster component for notifications in layout

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -1,4 +1,5 @@
 import "@/app/globals.css";
+import { Toaster } from "@/components/ui/sonner";
 import { Metadata } from "next";
 import { Poppins } from "next/font/google";
 
@@ -16,7 +17,11 @@ export const metadata: Metadata = {
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="eng">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        {children}
+        {/* Toast container */}
+        <Toaster richColors position="bottom-left" />
+      </body>
     </html>
   );
 }
