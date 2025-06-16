1b8245c62fa7a7bff4783bb7f1ad13db7adc2be2
wassii-khan <150877571+wassii-khan-git@users.noreply.github.com>
2025-06-16 19:48:08 +0500

    feat: add logic

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -5,6 +5,21 @@ import { Toaster } from "@/components/ui/sonner";
 import { AuthProvider } from "@/lib/provider";
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 import Navbar from "./(components)/navbar";
 
 const openSans = Open_Sans({
@@ -29,7 +44,23 @@ export default async function RootLayout({
       <body className={`${openSans.variable} antialiased`}>
         {!session && redirect("/")}
         <AuthProvider>
-          {children}
+          <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset>
+              <header className="relative flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+                <div className="flex w-full items-center gap-2 px-4">
+                  <SidebarTrigger className="-ml-1" />
+                  <Separator
+                    orientation="vertical"
+                    className="mr-2 data-[orientation=vertical]:h-4"
+                  />
+                  <Navbar />
+                </div>
+              </header>
+              <div className="mt-5 md:mx-auto">{children}</div>
+            </SidebarInset>
+          </SidebarProvider>
+
           {/* Toast container */}
           <Toaster richColors position="bottom-left" />
         </AuthProvider>
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,35 +1,5 @@
-"use client";
-import { useState } from "react";
-import Navbar from "./(components)/navbar";
 import Note from "./(components)/note";
-import Sidebar from "./(components)/sidebar";
 
-// Home Page
-export default function Home() {
-  const [isMenuClicked, setIsMenuClicked] = useState<boolean>(false);
-
-  return (
-    <>
-      {/* Navbar */}
-      <header>
-        <Navbar
-          setIsMenuClicked={setIsMenuClicked}
-          isMenuClicked={isMenuClicked}
-        />
-      </header>
-      <div className="flex ">
-        <aside
-          className={`${
-            isMenuClicked ? "w-20" : ""
-          }  transition-all ease-in-out duration-300`}
-        >
-          <Sidebar isMenuClicked={isMenuClicked} />
-        </aside>
-        <main className=" container mx-auto p-4 mt-10">
-          {/* add note */}
-          <Note />
-        </main>
-      </div>
-    </>
-  );
+export default function Page() {
+  return <Note />;
 }
