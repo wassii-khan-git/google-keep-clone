9e5726e0b42f497b0ce9f5859d30c2a8bf19912e
wassii-khan <150877571+wassii-khan-git@users.noreply.github.com>
2025-06-18 19:57:00 +0500

    feat: update custom tooltip

--- components/ui/custom-tooltip.tsx
+++ components/ui/custom-tooltip.tsx
@@ -24,14 +24,14 @@ const TooltipButton = ({
     >
       <label
         className={` ${
-          idx !== 1 ? "hover:bg-gray-100 p-0" : ""
+          idx !== 1 ? "hover:bg-gray-100 p-0 hover:text-black" : ""
         }  text-gray-500 px-3 py-2.5 rounded-full cursor-pointer`}
         onClick={onClick}
       >
         {icon}
       </label>
       <div
-        className={`absolute left-1/2 -translate-x-1/2 mt-5 w-max px-2 py-1 bg-gray-800 text-white text-xs rounded ${
+        className={`absolute left-1/2 -translate-x-1/2 mt-5 w-max px-2 py-1 hover:text-black bg-gray-800 text-white text-xs rounded ${
           isHovered ? "opacity-100" : "opacity-0"
         } transition-opacity pointer-events-none z-50`}
       >
