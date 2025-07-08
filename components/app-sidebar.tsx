a651680d6a043b4884dadafc40f845a65f7d2363
wassii-khan <wassiikhan933@gmail.com>
2025-07-09 01:02:24 +0500

    fix: refactor AppSidebar to improve layout and add tooltip for collapsed state

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,13 +1,7 @@
 "use client";
 
 import * as React from "react";
-import {
-  Bot,
-  LayoutDashboard,
-  LucideNotepadText,
-  NotepadTextIcon,
-  SquareTerminal,
-} from "lucide-react";
+import { Bot, GitFork, LayoutDashboard, LucideNotepadText } from "lucide-react";
 
 import { NavMain } from "@/components/nav-main";
 import { LogoHeader } from "@/components/team-switcher";
@@ -17,7 +11,9 @@ import {
   SidebarFooter,
   SidebarHeader,
   SidebarRail,
+  useSidebar,
 } from "@/components/ui/sidebar";
+import TooltipButton from "./ui/custom-tooltip";
 
 // This is sample data.
 const data = {
@@ -46,15 +42,26 @@ const data = {
 };
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const { state } = useSidebar();
   return (
     <Sidebar collapsible="icon" {...props}>
-      <SidebarHeader>
+      <SidebarHeader className="border-b border-gray-200 pb-4">
         <LogoHeader teams={data.logoInfo} />
       </SidebarHeader>
       <SidebarContent>
         <NavMain items={data.navMain} />
       </SidebarContent>
-      <SidebarFooter>All rights reserved</SidebarFooter>
+      <SidebarFooter className="text-center">
+        {state === "collapsed" ? (
+          <TooltipButton
+            icon={<GitFork className="text-gray-500" />}
+            tooltipText="All Rights Reserved"
+            isClickable={false}
+          />
+        ) : (
+          "All Rights Reserved"
+        )}
+      </SidebarFooter>
       <SidebarRail />
     </Sidebar>
   );
